64e161140a3c4535d8f5b2e27f857ba9c4e42d62
rafin5257 <rafin5257@gmail.com>
2026-04-04 05:46:28 +0000

    Add files via upload

---
+++ backupdots/waybar/scripts/chwal.sh
@@ -0,0 +1,8 @@
+
+WALLPAPER_DIR="$HOME/wallpapers"
+
+
+RANDOM_IMAGE=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+
+
+awww img "$RANDOM_IMAGE" --transition-type fade --transition-fps 60 --transition-step 2

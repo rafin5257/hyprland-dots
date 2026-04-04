9f65f8429b70e73d626dbd24c5945abad34e25b0
Rafin <rafin5257@gmail.com>
2026-04-04 12:31:00 +0000

    Add support for GIF images in wallpaper selection

--- backupdots/waybar/scripts/chwal.sh
+++ backupdots/waybar/scripts/chwal.sh
@@ -2,7 +2,7 @@
 WALLPAPER_DIR="$HOME/wallpapers"
 
 
-RANDOM_IMAGE=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+RANDOM_IMAGE=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" \) | shuf -n 1)
 
 
 awww img "$RANDOM_IMAGE" --transition-type fade --transition-fps 60 --transition-step 2
